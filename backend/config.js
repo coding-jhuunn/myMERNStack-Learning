764e45eeb21b599e0825826099c01dda841b432f
coding-jhuunn <codingatjhunn@gmail.com>
2024-02-29 14:57:00 +0800

    add ADD function and GET functions

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+export const MONGO_URL =
+  "mongodb+srv://1234:admin123@mydatabase.uvzngkb.mongodb.net/?retryWrites=true&w=majority&appName=myDatabase";
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,55 @@
-import express from "express";
-import { PORT } from "./config.js";
-
+import express, { response } from "express";
+import mongoose from "mongoose";
+import { PORT, MONGO_URL } from "./config.js";
+import { Book } from "./models/bookModels.js";
 const app = express();
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log(req);
+  return res.status(234).send("Welcome to MErn ");
 });
+
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return response
+        .status(400)
+        .send("send all required files:title author publsihYear");
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+    const book = await Book.create(newBook);
+    return res.status(200).send(book);
+  } catch (error) {
+    console.log(error);
+    return response.status(500).send({ message: error.message });
+  }
+});
+
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error);
+    return response.status(500).send({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(MONGO_URL)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => console.log(err));
